0105369c9b7adb2dae07e356468e9aca459bae72
OkejoTM <denastytiko03@mail.ru>
2021-09-12 22:39:01 +0300

    text
    text

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import App_style from './App_style.css'
+
+ReactDOM.render(
+  <div class = "divContainer">
+    <h1> Click on this </h1>
+    <button class = "buttonFirst"> Button </button> 
+  </div>,
+  document.getElementById('root')
+);
+
+
